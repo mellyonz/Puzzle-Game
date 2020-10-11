79895b0f90774e0c84d659d09c687f5033f7f94c
Kieran Abelen <kja0242@arastudent.ac.nz>
2020-10-12 12:04:54 +1300

    Dynamic Camera
    The camera currently is static has a fixed size, some global variables and logic need to be set up to control this. The size needs to adjust based on the grid size, but UI elements should not change in size but adjust when the camera zooms out. This means a global unit variable needs to be set up updated when the camera scales the UI scales too.

--- Scripts/MainGame.cs
+++ Scripts/MainGame.cs
@@ -18,6 +18,8 @@ public class MainGame : MonoBehaviour
     public string name;
   }
 
+  public Camera cameraMain;
+
   public Canvas dialogBox;
 
   // Create the a global level that will be load the json file into
@@ -37,36 +39,74 @@ public class MainGame : MonoBehaviour
   // Start is called before the first frame update
   void Start()
   {
-    // Read the json file into a local string 
+   b  // Read the json file into a local string 
     string json = File.ReadAllText(UnityEngine.Application.dataPath + "/Scripts/Maps/Map1.json");
-    //Debug.Log(json);
+    // Debug.Log(json);
 
+    // Build json string into an object
     level = JsonUtility.FromJson<Level>(json);
-    //Debug.Log(level.tileArray[0]);
-    //level.tileCorrect = new int[level.width * level.height];
-    //level.tileSelected = new int[level.width * level.height];
 
-    // Simple loop for the coordinates
-    for (int x = 0; x < level.width; x++)
+    // Sets the main camera  to the same size at the level height
+    cameraMain.orthographicSize = level.height / 2;
+    //view.orthographicSize = 20;
+
+    // Loop for the coordinates
+    for (int y = 0; y < level.height; y++)
     {
-      for (int y = 0; y < level.height; y++)
+      for (int x = 0; x < level.width; x++)
       {
         // The correct x and y index in the array is the height time the max width plus the width since for everything max width you will get a new row
         int levelCell = y * level.width + x;
 
         // Create a new asset inside the game to break its reference to the origin
-        tileBasic.gameObject = Instantiate(tileNumber, transform);
+        tileBasic.gameObject = Instantiate(tileNumber, new Vector3(0, 0, -20), Quaternion.identity);
 
         // Prefab can now be instanced without changing the other tiles prefabs
         TextMesh number = tileBasic.gameObject.GetComponentInChildren<TextMesh>();
 
-        // Simple logic since 0 is default in array
-        if (level.tileType[levelCell] != 0)
+        // Logic for tile type 1
+        if (level.tileType[levelCell] == 1)
         {
-          number.text = level.tileType[levelCell].ToString();
+          int tileNumberCorrect = 0;
+          for (int xmax = 0; xmax < level.width; xmax++)
+          {
+            int logic = levelCell + xmax;
+            if (0 < logic && logic < level.width * level.height)
+            {
+              //Debug.Log(logic);
+              if (level.tileCorrect[logic] == 1) { tileNumberCorrect++; }
+            }
+          }
+          for (int ymax = 0; ymax < level.height; ymax++)
+          {
+            int logic = levelCell - ymax * level.width;
+            if (0 < logic && logic <=  level.width * level.height)
+            {
+              //Debug.Log(logic);
+              if (level.tileCorrect[logic] == 1) { tileNumberCorrect++; }
+            }
+          }
+          number.text = tileNumberCorrect.ToString();
         }
 
+        // Logic for tile type 2
+        if (level.tileType[levelCell] == 2)
+        {
+          int tileNumberCorrect = 0;
+          for (int xdif = -1; xdif < 2; xdif++)
+          {
+            for (int ydif = -1; ydif < 2; ydif++)
+            {
+              int logic = (y + ydif) * level.width + x + xdif;
+              if (0 < logic && logic < level.width * level.height) {
+                if (level.tileCorrect[logic] == 1) { tileNumberCorrect++; }
+              }
+            }
+          }
+          number.text = tileNumberCorrect.ToString();
+        }
 
+        // Set tile sprite setting based of the first column and last row
         if (x == 0 && y != level.height - 1) 
         {
           tileBasic.sprite = square2;
@@ -78,19 +118,25 @@ public class MainGame : MonoBehaviour
           tileBasic.sprite = square1;
         }
 
-        // This is another instantiate but all the changes need be in place before creation
-        mapClick.SetTile(new Vector3Int(x, y, 0), tileBasic);
+        // Centre the tiles rather than using the top right quadrant
+        int xCentre = x - level.width / 2;
+        int yCentre = y - level.height / 2;
 
+        // This is another instantiate but all the changes need be in place before its creation
+        mapClick.SetTile(new Vector3Int(xCentre, yCentre, 0), tileBasic);
+        
+        // Checking level for a currently selected tile, will be used in save files
         if (level.tileSelected[levelCell] == 1)
         {
-          SetTileColour(Color.red, new Vector3Int(x, y, 0));
+          SetTileColour(Color.red, new Vector3Int(xCentre, yCentre, 0));
         }
-          // Destroy the original object after the launch
-          Destroy(tileBasic.gameObject, 1);
+
+        // Destroy the original object after the launch
+        Destroy(tileBasic.gameObject, 1);
       };
     };
   }
-
+   
   // Update will just listen to mouse clicks
   void Update()
   {
@@ -98,58 +144,70 @@ public class MainGame : MonoBehaviour
     if (Input.GetMouseButtonDown(0) && !Input.GetMouseButtonUp(0))
     {
       // Mouse position to camera position
-      Vector3 pos = Input.mousePosition;
-      pos = Camera.main.ScreenToWorldPoint(pos);
+      Vector3 mouse_pos = Input.mousePosition;
+      mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
 
       // Return tileMap x and y (x, y, z) based on the clicked tile position
-      Vector3Int idClick = GetIdCell(pos);
+      Vector3Int tileVector_int = TileVector_get(mouse_pos);
       //Debug.Log(idClick);
 
       // Set color to red if tile is white else color is white
-      if (mapClick.GetColor(idClick) == Color.white)
+      if (mapClick.GetColor(tileVector_int) == Color.white)
       {
-        SetTileColour(Color.red, idClick);
-        level.tileSelected[idClick.y * level.width + idClick.x] = 1;
+        SetTileColour(Color.red, tileVector_int);
+        level.tileSelected[LevelArrayIndex(tileVector_int)] = 1;
       }
       else
       {
-        SetTileColour(Color.white, idClick);
-        level.tileSelected[idClick.y * level.width + idClick.x] = 0;
+        SetTileColour(Color.white, tileVector_int);
+        level.tileSelected[LevelArrayIndex(tileVector_int)] = 0;
       }
     }
 
-
+    // Check if you Won by comparing correct tiles with selected tiles, else save the game
     if(level.tileSelected.SequenceEqual(level.tileCorrect)) 
     {
-      Debug.Log("Won");
+      //Debug.Log("Won");
       dialogBox.gameObject.SetActive(true);
-    }
-    else
+    }else
     { 
       string json = JsonUtility.ToJson(level);
-      File.WriteAllText(UnityEngine.Application.dataPath + "/Scripts/Map1.json", json);
-      Debug.Log("Not Won");
+      File.WriteAllText(UnityEngine.Application.dataPath + "/Scripts/Maps/Map1.json", json);
+      //Debug.Log("Not Won");
     }
   }
 
   // Basic functions
-  Vector3Int GetIdCell(Vector3 pos)
+  Vector3Int TileVector_get(Vector3 levelPos)
   {
     // Return the tile that is clicked
-    Vector3Int cellPosition = mapClick.WorldToCell(pos);
-
-    // Make sure its in the game screen but also exclude the left coloumn and top row from selection
-    if (cellPosition.x >= level.width || cellPosition.y >= level.height - 1 || cellPosition.x < 1 || cellPosition.y < 0)
+    Vector3Int tilePos = mapClick.WorldToCell(levelPos);
+    
+    // Make sure its in the game screen but also exclude the left coloumn and top row from selection, it is adjusted centering it like a graph
+    if (tilePos.x >= level.width / 2 || tilePos.y >= level.height / 2 - 1 || tilePos.x < -level.width / 2 + 1 || tilePos.y < -level.height / 2)
     {
-      cellPosition = new Vector3Int(-1, -1, -1);
+      // An unreachable tile
+      tilePos = new Vector3Int(0, 0, -1);
     }
 
-    return cellPosition;
+    // Return it as an int vector, this is needed to change colors
+    return tilePos;
+  }
+
+  int LevelArrayIndex(Vector3 tilePos)
+  {
+    // Turning all negative tile positions into positive
+    tilePos.x += level.width / 2;
+    tilePos.y = (tilePos.y + level.height / 2) * level.width;
+
+    int levelIndex = (int)(tilePos.x + tilePos.y);
+
+    // Returning the index of the level
+    return levelIndex;
   }
 
 
-
-  // Manipulate TileMaps 
+  // Manipulate TileMaps
   private void SetTileColour(Color colour, Vector3Int position)
   {
     // Flag the tile, inidicating that it can change colour.
