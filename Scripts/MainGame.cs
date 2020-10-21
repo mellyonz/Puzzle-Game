9998ea4328eb5fb43cbefe63eda6758836f699cc
Kieran Abelen <kja0242@arastudent.ac.nz>
2020-10-22 10:32:40 +1300

    Screens and Popup
    Brief
    This section requires a lot of thinking and explaining since one this will set a precedent for the entire game going ford. Second this will directly impact how the UI will interact with the main game script.
    Problem?
    The real question is do I separate the menus into its own scripts and or scenes?
    The simplest solution would be to keep everything together, but it would conflict with the start and update inside my main game script. The grid when launching the game should not be there until a level is selected.
    The next option is to separate the scripts but not the scene. This is a good option since we can enable the script in the scene at any time, and a pause the game which will have the same menu. The downside is that we will have two-way dependencies.
    The last option is that we separate all logic into their own script and scene. This would mean we would have to make another in game menu or force the player to exit to the main menu to change the level.
    
    I believe for now the best option is the simplest keeping everything in the same script. This would let me expand the functions of the base script and ruse many functions. A select level button could be using  the same function that is used to load a level from a previous save

--- Scripts/MainGame.cs
+++ Scripts/MainGame.cs
@@ -1,13 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 using Tile = UnityEngine.Tilemaps.Tile;
 
 public class MainGame : MonoBehaviour
-{
-  [Serializable]
+{ 
+
+  public Camera UICameraMain;
+
+  public Canvas UIMenu;
+  public RectTransform UIMenuMain; 
+  public RectTransform UIMenuSelect;
+  public RectTransform UIMenuSelectPanel;
+  public RectTransform UIMenuPopup; 
+
+  public Button UIButtonPopuptoStart;
+  public Button UIButtonPopuptoMain;
+  public Button UIButtonPopuptoSelect;
+  public Button UIButtonMaintoStart;
+  public Button UIButtonMaintoSelect;
+  public Button UIButtonLeveltoMain;
+  public GameObject UIButtonSelect; 
+
+  // Create the a global level that will be load the json file into
+    [Serializable]
   private class Level
   {
     public int width;
@@ -17,163 +37,65 @@ public class MainGame : MonoBehaviour
     public int[] tileSelected;
     public string name;
   }
-
-  public Camera cameraMain;
-
-  public Canvas dialogBox;
-
-  // Create the a global level that will be load the json file into
   Level level = new Level();
+  private bool levelActive_state;
+
+  private DirectoryInfo mapDirectory;
+  private FileInfo[] fis;
 
   // Main game Tilemap
   public Tilemap mapClick;
 
-  // Tilemap tiles objects
   public GameObject tileNumber;
   public Tile tileBasic;
   public Sprite square1;
   public Sprite square2;
   public Sprite square3;
+  // Tilemap tiles objects
 
-
-  // Start is called before the first frame update
   void Start()
   {
-   b  // Read the json file into a local string 
-    string json = File.ReadAllText(UnityEngine.Application.dataPath + "/Scripts/Maps/Map1.json");
-    // Debug.Log(json);
+    // Main menu button listeners
+    UIButtonMaintoStart.onClick.AddListener(delegate { LevelLoad(0); });
+    UIButtonMaintoSelect.onClick.AddListener(UISelect_transition); 
 
-    // Build json string into an object
-    level = JsonUtility.FromJson<Level>(json);
-
-    // Sets the main camera  to the same size at the level height
-    cameraMain.orthographicSize = level.height / 2;
-    //view.orthographicSize = 20;
-
-    // Loop for the coordinates
-    for (int y = 0; y < level.height; y++)
+    // Select level menu btton listeners
+    mapDirectory = new DirectoryInfo(Application.dataPath + "/Scripts/Maps");
+    fis = mapDirectory.GetFiles();
+    foreach (FileInfo fi in fis)
     {
-      for (int x = 0; x < level.width; x++)
+      if (fi.Extension.Contains("json"))
       {
-        // The correct x and y index in the array is the height time the max width plus the width since for everything max width you will get a new row
-        int levelCell = y * level.width + x;
-
-        // Create a new asset inside the game to break its reference to the origin
-        tileBasic.gameObject = Instantiate(tileNumber, new Vector3(0, 0, -20), Quaternion.identity);
-
-        // Prefab can now be instanced without changing the other tiles prefabs
-        TextMesh number = tileBasic.gameObject.GetComponentInChildren<TextMesh>();
-
-        // Logic for tile type 1
-        if (level.tileType[levelCell] == 1)
-        {
-          int tileNumberCorrect = 0;
-          for (int xmax = 0; xmax < level.width; xmax++)
-          {
-            int logic = levelCell + xmax;
-            if (0 < logic && logic < level.width * level.height)
-            {
-              //Debug.Log(logic);
-              if (level.tileCorrect[logic] == 1) { tileNumberCorrect++; }
-            }
-          }
-          for (int ymax = 0; ymax < level.height; ymax++)
-          {
-            int logic = levelCell - ymax * level.width;
-            if (0 < logic && logic <=  level.width * level.height)
-            {
-              //Debug.Log(logic);
-              if (level.tileCorrect[logic] == 1) { tileNumberCorrect++; }
-            }
-          }
-          number.text = tileNumberCorrect.ToString();
-        }
-
-        // Logic for tile type 2
-        if (level.tileType[levelCell] == 2)
-        {
-          int tileNumberCorrect = 0;
-          for (int xdif = -1; xdif < 2; xdif++)
-          {
-            for (int ydif = -1; ydif < 2; ydif++)
-            {
-              int logic = (y + ydif) * level.width + x + xdif;
-              if (0 < logic && logic < level.width * level.height) {
-                if (level.tileCorrect[logic] == 1) { tileNumberCorrect++; }
-              }
-            }
-          }
-          number.text = tileNumberCorrect.ToString();
-        }
-
-        // Set tile sprite setting based of the first column and last row
-        if (x == 0 && y != level.height - 1) 
-        {
-          tileBasic.sprite = square2;
-        }else if(y == level.height-1 && x != 0)
-        {
-          tileBasic.sprite = square3;
-        }else
-        {
-          tileBasic.sprite = square1;
-        }
-
-        // Centre the tiles rather than using the top right quadrant
-        int xCentre = x - level.width / 2;
-        int yCentre = y - level.height / 2;
-
-        // This is another instantiate but all the changes need be in place before its creation
-        mapClick.SetTile(new Vector3Int(xCentre, yCentre, 0), tileBasic);
-        
-        // Checking level for a currently selected tile, will be used in save files
-        if (level.tileSelected[levelCell] == 1)
-        {
-          SetTileColour(Color.red, new Vector3Int(xCentre, yCentre, 0));
-        }
-
-        // Destroy the original object after the launch
-        Destroy(tileBasic.gameObject, 1);
-      };
-    };
-  }
-   
-  // Update will just listen to mouse clicks
-  void Update()
-  {
-    // Is the mouse button down and not up?
-    if (Input.GetMouseButtonDown(0) && !Input.GetMouseButtonUp(0))
-    {
-      // Mouse position to camera position
-      Vector3 mouse_pos = Input.mousePosition;
-      mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
-
-      // Return tileMap x and y (x, y, z) based on the clicked tile position
-      Vector3Int tileVector_int = TileVector_get(mouse_pos);
-      //Debug.Log(idClick);
-
-      // Set color to red if tile is white else color is white
-      if (mapClick.GetColor(tileVector_int) == Color.white)
-      {
-        SetTileColour(Color.red, tileVector_int);
-        level.tileSelected[LevelArrayIndex(tileVector_int)] = 1;
-      }
-      else
-      {
-        SetTileColour(Color.white, tileVector_int);
-        level.tileSelected[LevelArrayIndex(tileVector_int)] = 0;
+        GameObject goButton = Instantiate(UIButtonSelect);
+        UnityEngine.UI.Button goButton2 = gameObject.AddComponent<UnityEngine.UI.Button>();
+        goButton.GetComponentInChildren<Button>().onClick.AddListener(delegate { LevelLoad(0); });
+        goButton.transform.SetParent(UIMenuSelectPanel, false);
       }
     }
+    UIButtonLeveltoMain.onClick.AddListener(UIMainMenu_transition);
 
-    // Check if you Won by comparing correct tiles with selected tiles, else save the game
-    if(level.tileSelected.SequenceEqual(level.tileCorrect)) 
+    // Popup menu button listeners
+    UIButtonPopuptoStart.onClick.AddListener(delegate { LevelLoad(0); });
+    UIButtonPopuptoMain.onClick.AddListener(UIMainMenu_transition);
+    UIButtonPopuptoSelect.onClick.AddListener(UISelect_transition);
+
+
+    // Transition screens
+    UIMenuMain.gameObject.SetActive(true);
+    UIMenuSelect.gameObject.SetActive(false);
+    UIMenuPopup.gameObject.SetActive(false);
+    levelActive_state = false;
+  }
+
+  void Update()
+  {
+    if (levelActive_state == true)
     {
-      //Debug.Log("Won");
-      dialogBox.gameObject.SetActive(true);
-    }else
-    { 
-      string json = JsonUtility.ToJson(level);
-      File.WriteAllText(UnityEngine.Application.dataPath + "/Scripts/Maps/Map1.json", json);
-      //Debug.Log("Not Won");
+      UIMenuMain.gameObject.SetActive(false);
+      UIMenuSelect.gameObject.SetActive(false);
+      UIMenuPopup.gameObject.SetActive(false);
+      LevelUpdate();
+     // Debug.Log("complete");
     }
   }
 
@@ -193,8 +115,7 @@ public class MainGame : MonoBehaviour
     // Return it as an int vector, this is needed to change colors
     return tilePos;
   }
-
-  int LevelArrayIndex(Vector3 tilePos)
+  int LevelArrayIndex_create(Vector3 tilePos)
   {
     // Turning all negative tile positions into positive
     tilePos.x += level.width / 2;
@@ -206,6 +127,168 @@ public class MainGame : MonoBehaviour
     return levelIndex;
   }
 
+  // UI Transitions
+  public void UIMainMenu_transition()
+  {
+    UIMenuMain.gameObject.SetActive(true);
+    UIMenuSelect.gameObject.SetActive(false);
+    UIMenuPopup.gameObject.SetActive(false);
+  }
+  public void UISelect_transition()
+  {
+    UIMenuMain.gameObject.SetActive(false);
+    UIMenuSelect.gameObject.SetActive(true);
+    UIMenuPopup.gameObject.SetActive(false);
+  }
+
+  // Manipulate Level
+  public void LevelLoad(int l)
+  {
+    Debug.Log(l);
+
+    //fis = mapDirectory.GetFiles();
+
+    // Read the json file into a local string 
+    string json = File.ReadAllText(fis[l].ToString());
+    // Debug.Log(json);
+
+    // Build json string into an object
+    level = JsonUtility.FromJson<Level>(json);
+
+    // Sets the main camera  to the same size at the level height
+    UICameraMain.orthographicSize = level.height / 2;
+    //view.orthographicSize = 20;
+
+    // Loop for the coordinates
+    for (int y = 0; y<level.height; y++)
+    {
+      for (int x = 0; x<level.width; x++)
+      {
+        // The correct x and y index in the array is the height time the max width plus the width since for everything max width you will get a new row
+        int levelCell = y * level.width + x;
+
+        // Create a new asset inside the game to break its reference to the origin
+        tileBasic.gameObject = Instantiate(tileNumber, new Vector3(0, 0, -20), Quaternion.identity);
+
+        // Prefab can now be instanced without changing the other tiles prefabs
+        TextMesh number = tileBasic.gameObject.GetComponentInChildren<TextMesh>();
+
+        // Logic for tile type 1
+        if (level.tileType[levelCell] == 1)
+        {
+          int tileNumberCorrect = 0;
+          for (int xmax = 0; xmax<level.width; xmax++)
+          {
+            int logic = levelCell + xmax;
+            if (0 < logic && logic<level.width* level.height)
+            {
+              //Debug.Log(logic);
+              if (level.tileCorrect[logic] == 1) { tileNumberCorrect++; }
+            }
+          }
+          for (int ymax = 0; ymax < level.height; ymax++)
+          {
+            int logic = levelCell - ymax * level.width;
+            if (0 < logic && logic <= level.width * level.height)
+            {
+              //Debug.Log(logic);
+              if (level.tileCorrect[logic] == 1) { tileNumberCorrect++; }
+            }
+          }
+          number.text = tileNumberCorrect.ToString();
+        }
+
+        // Logic for tile type 2
+        if (level.tileType[levelCell] == 2)
+        {
+          int tileNumberCorrect = 0;
+          for (int xdif = -1; xdif < 2; xdif++)
+          {
+            for (int ydif = -1; ydif < 2; ydif++)
+            {
+              int logic = (y + ydif) * level.width + x + xdif;
+              if (0 < logic && logic < level.width * level.height)
+              {
+                if (level.tileCorrect[logic] == 1) { tileNumberCorrect++; }
+              }
+            }
+          }
+          number.text = tileNumberCorrect.ToString();
+        }
+
+        // Set tile sprite setting based of the first column and last row
+        if (x == 0 && y != level.height - 1)
+        {
+          tileBasic.sprite = square2;
+        }
+        else if (y == level.height - 1 && x != 0)
+        {
+          tileBasic.sprite = square3;
+        }
+        else
+        {
+          tileBasic.sprite = square1;
+        }
+
+        // Centre the tiles rather than using the top right quadrant
+        int xCentre = x - level.width / 2;
+        int yCentre = y - level.height / 2;
+
+        // This is another instantiate but all the changes need be in place before its creation
+        mapClick.SetTile(new Vector3Int(xCentre, yCentre, 0), tileBasic);
+
+        // Checking level for a currently selected tile, will be used in save files
+        if (level.tileSelected[levelCell] == 1)
+        {
+          SetTileColour(Color.red, new Vector3Int(xCentre, yCentre, 0));
+        }
+
+        // Destroy the original object after the launch
+        Destroy(tileBasic.gameObject, 1);
+      };
+    };
+    levelActive_state = true;
+  }
+  private void LevelUpdate()
+  {
+    // Is the mouse button down and not up
+    if (Input.GetMouseButtonDown(0) && !Input.GetMouseButtonUp(0))
+    {
+      // Mouse position to camera position 
+      Vector3 mouse_pos = Input.mousePosition;
+      mouse_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
+
+      // Return tileMap x and y (x, y, z) based on the clicked tile position
+      Vector3Int tileVector_int = TileVector_get(mouse_pos);
+      //Debug.Log(idClick);
+
+      // Set color to red if tile is white else color is white
+      if (mapClick.GetColor(tileVector_int) == Color.white)
+      {
+        SetTileColour(Color.red, tileVector_int);
+        level.tileSelected[LevelArrayIndex_create(tileVector_int)] = 1;
+      }
+      else
+      {
+        SetTileColour(Color.white, tileVector_int);
+        level.tileSelected[LevelArrayIndex_create(tileVector_int)] = 0;
+      }
+    }
+
+    // Check if you Won by comparing correct tiles with selected tiles, else save the game
+    if (level.tileSelected.SequenceEqual(level.tileCorrect))
+    {
+      // Debug.Log("Won");
+      UIMenuPopup.gameObject.SetActive(true);
+      mapClick.gameObject.SetActive(false);
+    }
+    else
+    {
+      string json = JsonUtility.ToJson(level);
+      File.WriteAllText(UnityEngine.Application.dataPath + "/Scripts/Maps/Map1.json", json);
+      // Debug.Log("Not Won");
+    }
+  }
 
   // Manipulate TileMaps
   private void SetTileColour(Color colour, Vector3Int position)
